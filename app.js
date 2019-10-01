cddeb62631607fe041955ebcde78d3e383d558cb
Ian Andrewson <ianandrewson@gmail.com>
2019-10-01 11:29:02 -0700

    added styling, added the quiz

--- app.js
+++ app.js
@@ -2,18 +2,39 @@
 import { isYes } from './functions.js';
 import { checkAnswer } from './functions.js';
 
-
 const myButton = document.getElementById('action-button');
-
+const answerField = document.getElementById('answer-field');
 
 myButton.onclick = () => {
-    // alert('welcome to my quiz!');
-    // const myConfirmation = confirm('Do you want to take my quiz?');
-    // console.log(myConfirmation);
-    const question1 = prompt('What is my favorite color?').toLowerCase().trim();
-    if (checkAnswer(1, question1)) {
+    alert('Welcome to my quiz!');
+    let score = 0;
+    const userName = prompt('What is your name?').trim();
+    const myConfirmation = confirm(`Would you like to take my quiz, ${userName}?`);
+    const question1Ans = prompt('Do I love surfing after breaking my face?').toLowerCase().trim();
+    if (checkAnswer(1, question1Ans)) {
         alert('Correct!');
+        score++;
     } else {
         alert('Wrong');
     }
+
+    const question2Ans = prompt(`Hey ${userName}, did I live in Copenhagen?`).toLowerCase().trim();
+    if (checkAnswer(2, question2Ans)) {
+        alert('Correct!');
+        score++;
+    } else {
+        alert('Wrong');
+    }
+
+    const question3Ans = prompt('Do I love synthesizers?').toLowerCase().trim();
+    if (checkAnswer(3, question3Ans)) {
+        alert('Correct!');
+        score++;
+    } else {
+        alert('Wrong');
+    }
+
+    alert(`You have finished the quiz, ${userName}! The results will now be displayed.`)
+
+    answerField.textContent = `You got ${score} correct.`
 };
--- functions.js
+++ functions.js
@@ -1,16 +1,17 @@
+export const isYes = userResponse => {
+    if (userResponse === 'yes' || userResponse === 'y' || userResponse === 'yup' || userResponse === 'yea' || userResponse === 'yeah' || userResponse === 'ya' || userResponse === 'jo') return true;
+    return false;
+}
+
 export const checkAnswer = (questionNumber, userInput) => {
-    if (questionNumber === 1 && userInput === 'yes') {
+    if (questionNumber === 1 && isYes(userInput)) {
         return true;
-    } else if (questionNumber === 2 && userInput === '') {
+    } else if (questionNumber === 2 && !isYes(userInput)) {
+        //Technically bugged; any input other than yes will give a correct answer, not just no
         return true;
-    } else if (questionNumber === 3 && userInput === '') {
+    } else if (questionNumber === 3 && isYes(userInput)) {
         return true;
     } else {
         return false;
     }
 };
-
-export const isYes = userResponse => {
-    if (userResponse === 'yes') return true;
-    return false;
-}
